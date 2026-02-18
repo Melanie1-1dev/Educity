993b412c19773e41ce7a0a5dee0882a75db70a5d
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-18 16:51:36 +0200

    Updated video

--- src/compnents/VideoPlayer/Videoplayer.jsx
+++ src/compnents/VideoPlayer/Videoplayer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Videoplayer.css'
 import video from '../../assets/college-video.mp4'
 const Videoplayer = ({playState, setPlayState}) => {
+  const player = useRef(null);
+  const closePlayer = ()=>{
+    if(e.target === player.current){
+      setPlayState(false);
+    }
+  }
   return (
-    <div className={`video-player ${playState?'':'hide'}`}>
+    <div className={`video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
       <video src={video} autoPlay muted controls></video>
     </div>
   )
